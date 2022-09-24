cfd1e2247bb7f5f0789959cd7685c4a7b47cb033
Chris Morin <longhumansbiz@gmail.com>
2022-09-24 17:33:02 -0300

    added journals

--- controllers/journalController.js
+++ controllers/journalController.js
@@ -1,10 +1,52 @@
+const Goals = require("../models/Goals")
+const Journals = require("../models/Journals")
+
+
+
+
 module.exports = {
-    getJournal: async (req,res)=>{
-        console.log(req.user)
-        try{
-            res.render('journal.ejs',)
-        }catch(err){
-            console.log(err)
+    getJournal: async (req, res) => {
+        try {
+          const goal = await Goals.findById(req.params.id);
+          const journals = await Journals.find( {goalId: req.params.id} )
+          res.render("journal.ejs", { 
+            goal: goal, 
+            user: req.user, 
+            journals: journals
+          });
+        } catch (err) {
+          console.log(err);
         }
-    },
+      },
+    createJournal: async (req, res) => {
+      try {
+          await Journals.create({
+            journalTitle: req.body.journalTitle.trim(),
+            journalText: req.body.journalText.trim(),
+            goalId: req.params.id, 
+
+          })
+          console.log('Journal Created')
+          res.redirect(`../${req.params.id}`)
+      }catch(err){
+        console.log(err)
+      }
+    }
 }
+
+
+
+
+
+
+
+// module.exports = {
+//     getJournal: async (req,res)=>{
+//         console.log(req.user)
+//         try{
+//             res.render('journal.ejs',)
+//         }catch(err){
+//             console.log(err)
+//         }
+//     },
+// }
---
+++ models/Journals.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+
+const JournalSchema = new mongoose.Schema({
+    journalTitle: {
+        type: String,
+        default: new Date().toLocaleDateString()
+    },
+    journalText: {
+        type: String,
+        required: true
+    },
+    journalDate: {
+        type: String,
+        default: new Date().toLocaleDateString(),
+        required: true,
+
+    },
+    goalId: {
+        type: String,
+        required: true
+    }
+})
+
+
+module.exports = mongoose.model('Journals', JournalSchema)
