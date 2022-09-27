24a02edc75f4fd9942b283c4780374428120981f
Chris Morin <longhumansbiz@gmail.com>
2022-09-27 16:11:35 -0300

    button and pomo styles

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -13,25 +13,25 @@ addTask.addEventListener('click', () => {
 //   const test = getComputedStyle(document.getElementsByClassName('sec'))
 //   .getPropertyValue('--value'); // #999999
 //   console.log(test)
-let minutesStyles = getComputedStyle(document.querySelector('.minutes'))
-let secondsStyles = getComputedStyle(document.querySelector('.seconds'))
+// let minutesStyles = getComputedStyle(document.querySelector('.minutes'))
+// let secondsStyles = getComputedStyle(document.querySelector('.seconds'))
 
-let minutesVal = minutesStyles.getPropertyValue(`--value`);
-let secondsVal = secondsStyles.getPropertyValue(`--value`);
+// let minutesVal = minutesStyles.getPropertyValue(`--value`);
+// let secondsVal = secondsStyles.getPropertyValue(`--value`);
 
 
-const timer = setInterval(function () {
-    document.querySelector('.seconds').style.setProperty(`--value`, secondsVal--)
+// const timer = setInterval(function () {
+//     document.querySelector('.seconds').style.setProperty(`--value`, secondsVal--)
 
-    if(secondsVal <= -1){
-        document.querySelector('.seconds').style.setProperty(`--value`, 59)
-        document.querySelector('.minutes').style.setProperty(`--value`, minutesVal-1)
+//     if(secondsVal <= -1){
+//         document.querySelector('.seconds').style.setProperty(`--value`, 59)
+//         document.querySelector('.minutes').style.setProperty(`--value`, minutesVal-1)
 
-        secondsVal = 59
+//         secondsVal = 59
 
-    }
+//     }
  
-},1000)
+// },1000)
         
        
   
